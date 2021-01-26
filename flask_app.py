bba2c9d07e87583a4e6614697b341f33abb80e31
Maxim Ladoshin <66252439+mladoshin@users.noreply.github.com>
2021-01-26 15:34:31 +0300

    Update flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -1,5 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template, request
 app = Flask(__name__)
 @app.route('/')
-def hello_world():
-    return 'Hello from Flask!'
+def index():
+    return render_template('index.html')
+
+@app.route('/endurance')
+def endurance():
+    return render_template('endurance.html')
+
+@app.route('/james_caird')
+def james_caird():
+    return render_template('james_caird.html')
+
+if __name__ == '__main__':
+    app.run(debug = True)
